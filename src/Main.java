e6f519197acd8c7b22a318ea2164e6a550695212
grifnmi <mgriffin37@universityofgalway.ie>
2023-10-19 14:23:35 +0100

    test 19th oct

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,8 @@ public class Main {
         System.out.printf("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Press Shift+F9 to start debugging your code. We have set one breakpoint
+        // for you, but you can always add more by pressing Ctrl+F8.
+        for (int i = 1; i <= 5; i++) System.out.println("i = " + i);
     }
 }
